7d3c22c48c7a289fa36444657282cb9bd519e4bb
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-06 23:47:46 +0200

    Added class

--- main.cpp
+++ main.cpp
@@ -14,160 +14,176 @@ using std::string;
 using std::vector;
 
 
+class rover {
+private:
+    
+    static constexpr double pi = 3.14159265;
 
-enum class Dir {E=0, W=90, N=180, S=270};
-enum class Move{L, R, M};
+    enum class Move{L, R, M};
 
-struct Pose {
-    int X;
-    int Y;
-    int theta;      // degrees
+    struct Pose {
+        int X;
+        int Y;
+        int theta;      // degrees
 
-    Pose(): X(0), Y(0), theta(0)  {}
+        Pose(): X(0), Y(0), theta(0)  {}
+    };
+
+    Pose pose_;
+    vector<Move> cmds_;
+    std::tuple<int, int> gridSize_;
+    
+public:
+
+    rover () {}
+    // rover (int x, int y, int theta) : pose_.X(x), pose_.Y(y), pose_.theta(theta) {}
+    void setGridSize(const int x, const int y) {
+        gridSize_ = std::make_tuple(x, y);
+    }
+
+    bool CheckValidCell(const Pose& pos, const std::tuple<int, int>& gridSize) {
+        bool on_grid_x = (pos.X >= 0 && pos.X < std::get<0>(gridSize));
+        bool on_grid_y = (pos.Y >= 0 && pos.Y < std::get<1>(gridSize));
+        if (on_grid_x && on_grid_y)
+            return true;
+        return false;
+    }
+
+    int convert_DirtoTheata(const char dir) {
+        if      (dir == 'E') return 0;
+        else if (dir == 'N') return 90;
+        else if (dir == 'W') return 180;
+        else if (dir == 'S') return 270;
+        else {
+            cout << " Direction not found !! \n";
+            return -1;
+        }
+    }
+
+    char convert_ThetatoDir(const int theta) {
+        if (theta == 0  || theta == 360) return 'E';
+        else if (theta == 90)   return 'N';
+        else if (theta == 180)  return 'W';
+        else if (theta == 270)  return 'S';
+        else {
+            cout << "theta is out of scope !! \n" ;
+            return 'x';
+        }
+    }
+
+    void parsePoseLine(string line) {
+        istringstream sline(line);
+        int x, y;
+        char c;
+        sline >> x >> y >> c ;
+        pose_.X = x;
+        pose_.Y = y;
+        pose_.theta = convert_DirtoTheata(c);
+        return;
+    }
+
+    void parseCmdsLine(string line) {
+        istringstream sline(line);
+        cmds_.clear();      // making sure nothing exists.
+        char c;
+        while (sline >> c ) {
+            if(c == 'L') cmds_.push_back(Move::L);
+            else if (c == 'R') cmds_.push_back(Move::R);
+            else if (c == 'M') cmds_.push_back(Move::M);
+            else cout << " Move command not found \n";
+        }
+        return;
+    }
+
+    int normalizeTheta(int theta){
+        theta = theta % 360 ;
+        if(theta < 0) {
+            theta += 360;
+        }
+        return theta;
+    }
+
+    void executeCommand(Pose &pose, Move cmd) {
+        switch (cmd)
+        {
+        case Move::L :
+            pose.theta += 90 ;
+            pose.theta = normalizeTheta(pose.theta);
+            break;
+        case Move::R :
+            pose.theta -= 90 ;
+            pose.theta = normalizeTheta(pose.theta);
+            break;
+        case Move::M :
+            pose.X += int(cos(pose.theta*pi/180)) ;
+            pose.Y += int(sin(pose.theta*pi/180)) ;
+            break;
+        default:
+            cout << "command not found !! " ;
+            break;
+        }
+    }
+
+    void executeCmds() {
+        for(auto cmd: cmds_)
+            executeCommand(pose_, cmd);
+    }
+
+    // Debug
+    void printcmd(const Move cmd){
+        switch (cmd)
+        {
+        case Move::L :
+            cout << "L ";
+            return;
+        case Move::R :
+            cout <<  "R ";
+            return;
+        case Move::M :
+            cout <<  "M ";
+            return;
+        default:
+            cout <<  "0 "; 
+            return;
+        }
+    }
+
+    void printCmds() {
+        for (auto cmd : cmds_)
+            printcmd(cmd);
+    }
+    // Debug
+    void printpose(const Pose p) {
+        cout <<p.X << " " << p.Y << " " << convert_ThetatoDir(p.theta)<< "\n";
+    }
+
+    void printCurrentPose() {
+        cout <<pose_.X << " " << pose_.Y << " " << convert_ThetatoDir(pose_.theta)<< "\n";
+    }
+
 };
 
-char convert_ThetatoDir(const int theta) {
-    if (theta == 0  || theta == 360) return 'E';
-    else if (theta == 90)   return 'N';
-    else if (theta == 180)  return 'W';
-    else if (theta == 270)  return 'S';
-    else {
-        cout << "theta is out of scope !! \n" ;
-        return 'x';
-    }
-}
-
-int convert_DirtoTheata(const char dir) {
-    if      (dir == 'E') return 0;
-    else if (dir == 'N') return 90;
-    else if (dir == 'W') return 180;
-    else if (dir == 'S') return 270;
-    else {
-        cout << " Direction not found !! \n";
-        return -1;
-    }
-}
-
-int normalizeTheta(int theta){
-    theta = theta % 360 ;
-    if(theta < 0) {
-        theta += 360;
-    }
-    return theta;
-}
-
-void printpose(const Pose p) {
-    cout <<p.X << " " << p.Y << " " << convert_ThetatoDir(p.theta)<< "\n";
-}
-void printcmd(const Move cmd){
-    switch (cmd)
-    {
-    case Move::L :
-        cout << "L ";
-        return;
-    case Move::R :
-        cout <<  "R ";
-        return;
-    case Move::M :
-        cout <<  "M ";
-        return;
-    default:
-        cout <<  "0 "; 
-        return;
-    }
-}
-
-Pose ParseposeLine(string line) {
-    istringstream sline(line);
-    int x, y;
-    char c;
-    sline >> x >> y >> c ;
-    Pose p;
-    p.X = x;
-    p.Y = y;
-    if      (c == 'E') p.theta = 0;
-    else if (c == 'N') p.theta = 90;
-    else if (c == 'W') p.theta = 180;
-    else if (c == 'S') p.theta = 270;
-    else cout << " Direction not found \n";
-    return p;
-}
-
-static double constexpr pi = 3.14159265;
-
-vector<Move> ParseCmdLine(string line) {
-    istringstream sline(line);
-    char c;
-    vector<Move> cmds;
-    while (sline >> c ) {
-        if(c == 'L') cmds.push_back(Move::L);
-        else if (c == 'R') cmds.push_back(Move::R);
-        else if (c == 'M') cmds.push_back(Move::M);
-        else cout << " Move command not found \n";
-    }
-    return cmds;
-}
-
-void executeCommand(Pose &pose, Move cmd) {
-    switch (cmd)
-    {
-    case Move::L :
-        // cout << "L " ;
-        pose.theta += 90 ;
-        pose.theta = normalizeTheta(pose.theta);
-        break;
-    case Move::R :
-        // cout << "R " ;
-        pose.theta -= 90 ;
-        pose.theta = normalizeTheta(pose.theta);
-        break;
-    case Move::M :
-        // cout << "M " ;
-        // cout << pose.theta << " " << int(cos(pose.theta*pi/180)) << " ";
-        pose.X += int(cos(pose.theta*pi/180)) ;
-        pose.Y += int(sin(pose.theta*pi/180)) ;
-        break;
-    default:
-        cout << "command not found !! " ;
-        break;
-    }
-}
 
 void ReadFile(string path) {
     ifstream myfile (path);
     if (myfile) {
+        rover r;
         string line;
         getline(myfile, line);
-        Pose p = ParseposeLine(line);
+        r.parsePoseLine(line);
+        r.printCurrentPose();
         getline(myfile, line);
-        vector<Move> cmds = ParseCmdLine(line);
-        cout << "Initial pose: \n";
-        cout << p.theta << " ";
-        printpose(p);
-        for(auto cmd : cmds)
-            printcmd(cmd);
+        r.parseCmdsLine(line);
+        r.printCmds();
         cout << "\n";
-        cout << "start of execution: \n" ;
-        for (auto cmd : cmds) {
-            executeCommand(p, cmd);
-            // cout << p.theta << " ";
-            printpose(p);
-        }
+        r.executeCmds();
+        r.printCurrentPose();
     }
 }
 
-// TODO: check if the cell is valid.
-bool CheckValidCell(const Pose& pos, const std::tuple<int, int>& gridSize) {
-    bool on_grid_x = (pos.X >= 0 && pos.X < std::get<0>(gridSize));
-    bool on_grid_y = (pos.Y >= 0 && pos.Y < std::get<1>(gridSize));
-
-}
 
 int main() 
 {   
-    Pose rover1_pose;
-    // Debug
-    // out << "X: " << rover1_pose.X << " Y: " << rover1_pose.Y << " dir: " ;
 
     ReadFile("cmd.txt");
 
