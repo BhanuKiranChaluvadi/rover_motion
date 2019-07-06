0f88018c9f81fb645e639e07fdb67750a4567ec4
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-07 00:48:18 +0200

    Added combined read function and main function.

--- main.cpp
+++ main.cpp
@@ -16,8 +16,6 @@ using std::vector;
 
 class rover {
 private:
-    
-    static constexpr double pi = 3.14159265;
 
     enum class Move{L, R, M};
 
@@ -29,17 +27,13 @@ private:
         Pose(): X(0), Y(0), theta(0)  {}
     };
 
+    static constexpr double pi = 3.14159265;
+
     Pose pose_;
     vector<Move> cmds_;
     std::tuple<int, int> gridSize_;
-    
-public:
 
-    rover () {}
-    // rover (int x, int y, int theta) : pose_.X(x), pose_.Y(y), pose_.theta(theta) {}
-    void setGridSize(const int x, const int y) {
-        gridSize_ = std::make_tuple(x, y);
-    }
+private:
 
     bool CheckValidCell(const Pose& pos, const std::tuple<int, int>& gridSize) {
         bool on_grid_x = (pos.X >= 0 && pos.X < std::get<0>(gridSize));
@@ -49,7 +43,7 @@ public:
         return false;
     }
 
-    int convert_DirtoTheata(const char dir) {
+    int convert_DirtoTheata(const char dir) const {
         if      (dir == 'E') return 0;
         else if (dir == 'N') return 90;
         else if (dir == 'W') return 180;
@@ -60,7 +54,7 @@ public:
         }
     }
 
-    char convert_ThetatoDir(const int theta) {
+    char convert_ThetatoDir(const int theta) const {
         if (theta == 0  || theta == 360) return 'E';
         else if (theta == 90)   return 'N';
         else if (theta == 180)  return 'W';
@@ -69,6 +63,61 @@ public:
             cout << "theta is out of scope !! \n" ;
             return 'x';
         }
+    }
+
+    int normalizeTheta(int theta){
+        theta = theta % 360 ;
+        if(theta < 0) {
+            theta += 360;
+        }
+        return theta;
+    }
+    void executeCommand(Pose &pose, Move cmd) {
+        switch (cmd)
+        {
+        case Move::L :
+            pose.theta += 90 ;
+            pose.theta = normalizeTheta(pose.theta);
+            break;
+        case Move::R :
+            pose.theta -= 90 ;
+            pose.theta = normalizeTheta(pose.theta);
+            break;
+        case Move::M :
+            pose.X += round(cos(pose.theta*pi/180)) ;
+            pose.Y += round(sin(pose.theta*pi/180)) ;
+            break;
+        default:
+            cout << "command not found !! " ;
+            break;
+        }
+    }
+    void printcmd(const Move cmd) const {
+        switch (cmd)
+        {
+        case Move::L :
+            cout << "L ";
+            return;
+        case Move::R :
+            cout <<  "R ";
+            return;
+        case Move::M :
+            cout <<  "M ";
+            return;
+        default:
+            cout <<  "0 "; 
+            return;
+        }
+    }
+
+
+public:
+
+    rover () {}
+    rover (int grid_x, int grid_y) : gridSize_(std::make_tuple (grid_x, grid_y)) {}
+
+    void setGridSize(const int x, const int y) {
+        gridSize_ = std::make_tuple(x, y);
     }
 
     void parsePoseLine(string line) {
@@ -95,97 +144,48 @@ public:
         return;
     }
 
-    int normalizeTheta(int theta){
-        theta = theta % 360 ;
-        if(theta < 0) {
-            theta += 360;
-        }
-        return theta;
-    }
-
-    void executeCommand(Pose &pose, Move cmd) {
-        switch (cmd)
-        {
-        case Move::L :
-            pose.theta += 90 ;
-            pose.theta = normalizeTheta(pose.theta);
-            break;
-        case Move::R :
-            pose.theta -= 90 ;
-            pose.theta = normalizeTheta(pose.theta);
-            break;
-        case Move::M :
-            pose.X += int(cos(pose.theta*pi/180)) ;
-            pose.Y += int(sin(pose.theta*pi/180)) ;
-            break;
-        default:
-            cout << "command not found !! " ;
-            break;
-        }
-    }
-
     void executeCmds() {
         for(auto cmd: cmds_)
             executeCommand(pose_, cmd);
     }
 
     // Debug
-    void printcmd(const Move cmd){
-        switch (cmd)
-        {
-        case Move::L :
-            cout << "L ";
-            return;
-        case Move::R :
-            cout <<  "R ";
-            return;
-        case Move::M :
-            cout <<  "M ";
-            return;
-        default:
-            cout <<  "0 "; 
-            return;
-        }
-    }
-
-    void printCmds() {
-        for (auto cmd : cmds_)
-            printcmd(cmd);
+    void printPose() const {
+        cout <<pose_.X << " " << pose_.Y << " " << convert_ThetatoDir(pose_.theta)<< "\n";
     }
     // Debug
-    void printpose(const Pose p) {
-        cout <<p.X << " " << p.Y << " " << convert_ThetatoDir(p.theta)<< "\n";
-    }
-
-    void printCurrentPose() {
-        cout <<pose_.X << " " << pose_.Y << " " << convert_ThetatoDir(pose_.theta)<< "\n";
+    void printCmds() const {
+        for (auto cmd : cmds_)
+            printcmd(cmd);
     }
 
 };
 
 
-void ReadFile(string path) {
+int main() 
+{   
+    string path = "cmd.txt";
     ifstream myfile (path);
     if (myfile) {
-        rover r;
+        // Get the first line and set grid size.
+        int grid_x, grid_y;
         string line;
         getline(myfile, line);
+        istringstream sline(line);
+        sline >> grid_x >> grid_y ;
+        rover r(grid_x, grid_y);
+        // get position line
+        getline(myfile, line);
         r.parsePoseLine(line);
-        r.printCurrentPose();
+        r.printPose();
+        // get commands line
         getline(myfile, line);
         r.parseCmdsLine(line);
         r.printCmds();
         cout << "\n";
+        // execute.
         r.executeCmds();
-        r.printCurrentPose();
+        r.printPose();
     }
-}
-
-
-int main() 
-{   
-
-    ReadFile("cmd.txt");
-
     return 0;
 }
