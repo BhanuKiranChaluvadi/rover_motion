4d4126f9c3488c7796665119edd4c0165d1d94e6
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-06 13:01:05 +0200

    Added enums for direction.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
+// g++ -std="c++17" -pthread main.cpp && ./a.out
+
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
 
 using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+enum class Dir {E, W, N, S};
+
+struct pose {
+    int X;
+    int Y;
+    Dir dir;
+
+    pose(): X(0), Y(0), dir(Dir::E)  {}
+};
+
 
 int main() 
-{
+{   
+    pose rover1_pose;
+    cout << "X: " << rover1_pose.X << " Y: " << rover1_pose.Y << " dir: " ;
+    if(rover1_pose.dir == Dir::E)
+        cout << "yes \n";
+    else
+        cout << "no \n";
     cout << "Rover test \n";
     return 0;
 }
