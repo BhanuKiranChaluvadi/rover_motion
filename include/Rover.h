83ddf26fb44b4a18cbbad896c2efe904393d8e14
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-07 07:46:56 +0200

    Added missing include and src files.

---
+++ include/Rover.h
@@ -0,0 +1,75 @@
+#ifndef ROVER_H_
+#define ROVER_H_
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <cmath>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+
+
+class Rover {
+private:
+
+    enum class Move{L, R, M};
+
+    struct Pose {
+        int X;
+        int Y;
+        int theta;      // degrees
+
+        Pose(): X(0), Y(0), theta(0)  {}
+    };
+
+    static constexpr double pi = 3.14159265;
+
+    Pose pose_;
+    vector<Move> cmds_;
+    std::tuple<int, int> gridSize_;
+
+private:
+
+    bool checkValidCell(const Pose& , const std::tuple<int, int>& );
+
+    int convert_DirtoTheata(const char) const ;
+
+    char convert_ThetatoDir(const int theta) const ;
+
+    int normalizeTheta(int theta);
+
+    void executeCommand(Pose &pose, Move cmd) ;
+
+    void printcmd(const Move cmd) const ;
+
+
+public:
+
+    rover () {}
+    rover (int grid_x, int grid_y) : gridSize_(std::make_tuple (grid_x, grid_y)) {}
+
+    void setGridSize(const int x, const int y) ;
+
+    void parsePoseLine(string line) ;
+
+    void parseCmdsLine(string line) ;
+
+    void executeCmds() ;
+
+    // Debug
+    void printPose() const ;
+    // Debug
+    void printCmds() const ;
+
+};  
+
+# endif // ROVER_H_
+
+
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+// g++ -std="c++17" -pthread main.cpp && ./a.out
+
+#include <iostream>
+
+#include "Rover.h"
+
+int main (int argc, const char **argv) {
+    // std::string input_file = "";
+    // if( argc > 1 ) {
+    //     for( int i = 1; i < argc; ++i )
+    //         if( std::string_view{argv[i]} == "-f" && ++i < argc )
+    //             input_file = argv[i];
+    // }
+    // else {
+    //     std::cout << "Usage: [executable] [-f input_file.txt]" << std::endl;    
+    // }
+
+    string path = "cmd.txt";
+    ifstream myfile (path);
+    if (myfile) {
+        // Get the first line and set grid size.
+        int grid_x, grid_y;
+        string line;
+        getline(myfile, line);
+        istringstream sline(line);
+        sline >> grid_x >> grid_y ;
+        Rover R(grid_x, grid_y);
+        // get position line
+        getline(myfile, line);
+        R.parsePoseLine(line);
+        R.printPose();
+        // get commands line
+        getline(myfile, line);
+        R.parseCmdsLine(line);
+        R.printCmds();
+        cout << "\n";
+        // execute.
+        R.executeCmds();
+        R.printPose();
+    }
+
+    
+}
