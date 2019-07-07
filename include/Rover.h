0f1cd4c5dfcd67393ead898d9cb00e7f893d1e7e
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-07 11:18:37 +0200

    Added class Grid.

--- include/Rover.h
+++ include/Rover.h
@@ -1,74 +1,42 @@
 #ifndef ROVER_H_
 #define ROVER_H_
 
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <tuple>
+#include <queue>
 #include <cmath>
-using std::cout;
-using std::ifstream;
-using std::istringstream;
-using std::string;
-using std::vector;
+using std::queue;
 
+#define PI 3.14159265
 
+enum class Move{L, R, M};
 
-class Rover {
-private:
+struct Position {
+    int X;
+    int Y;
+    int theta;      // degrees
+    Position(): X(0), Y(0), theta(0)  {}
+};
 
-    enum class Move{L, R, M};
-
-    struct Pose {
-        int X;
-        int Y;
-        int theta;      // degrees
-
-        Pose(): X(0), Y(0), theta(0)  {}
-    };
-
-    static constexpr double pi = 3.14159265;
-
-    Pose pose_;
-    vector<Move> cmds_;
-    std::tuple<int, int> gridSize_;
-
-private:
-
-    bool checkValidCell(const Pose& pos, const std::tuple<int, int>& gridSize);
-
-    int convert_DirtoTheata(const char) const ;
-
-    char convert_ThetatoDir(const int theta) const ;
-
-    int normalizeTheta(int theta);
-
-    void executeCommand(Pose &pose, Move cmd) ;
-
-    void printcmd(const Move cmd) const ;
-
-
+class Grid {
+    int x_bound;
+    int y_bound;
+    
+    Grid(int x, int y) {
+        this->x_bound = x;
+        this->y_bound = y;
+    }
+    
 public:
+    bool isValidPosition(int x, int y) {
+        if (x >= 0 && y >= 0 && x < x_bound && y < y_bound) {
+            return true;
+        }
+        return false;
+    }
+};
 
-    Rover () {}
-    Rover (int grid_x, int grid_y) : gridSize_(std::make_tuple (grid_x, grid_y)) {}
 
-    void setGridSize(const int x, const int y) ;
 
-    void parsePoseLine(string line) ;
 
-    void parseCmdsLine(string line) ;
-
-    void executeCmds() ;
-
-    // Debug
-    void printPose() const ;
-    // Debug
-    void printCmds() const ;
-
-};  
 
 # endif // ROVER_H_
 
