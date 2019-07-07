4bc4de936750a314009405f6aebd5d5b768c9968
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-07 19:19:28 +0200

    sample tests.

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,13 +1,29 @@
 #include "include/Rover.h"
-
+#include <iostream>
 #include <gtest/gtest.h>
 
-class ReverseTests : public ::testing::Test
-{
-};
 
-TEST_F(ReverseTests,  is_palindrome )
-{
-    EXPECT_TRUE(true);
+// TEST_F(RoverTests,  dirtoTheta)
+// {   
+//     char dir = 'E';
+//     EXPECT_EQ(0, Rover::convertDirToTheata(dir));
+//     dir = 'W';
+//     EXPECT_EQ(270, Rover::convertDirToTheata(dir));
+//     dir = 'N';
+//     EXPECT_EQ(90, Rover::convertDirToTheata(dir));
+//     dir = 'S';
+//     EXPECT_EQ(270, Rover::convertDirToTheata(dir));
+//     cout << Rover::convertDirToTheata(dir) << "\n";
 
-}
+//     std::cout << "HelloWorld !! \n";
+// }
+
+// TEST(RoverTests,  sample)
+// {   
+//     EXPECT_TRUE(true);
+// }
+
+// TEST(OtherRoverTests,  rover)
+// {   
+//     EXPECT_TRUE(true);
+// }
