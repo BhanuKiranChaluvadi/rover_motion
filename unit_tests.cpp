5beb86bb4eaf1fd35d49501c7df90227fcc117bc
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-07 09:52:33 +0200

    initial commit gunit test.

---
+++ unit_tests.cpp
@@ -0,0 +1,14 @@
+#include "include/Rover.h"
+
+#include <gtest/gtest.h>
+
+class ReverseTests : public ::testing::Test
+{
+};
+
+TEST_F(ReverseTests,  is_palindrome )
+{
+    Rover pally;
+    EXPECT_TRUE(true);
+
+}
