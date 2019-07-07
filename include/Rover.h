0eb87625c2eb794e5eb8b629a48225855fd75ffa
BhanuKiranChaluvadi <bc@blue-ocean-robotics.com>
2019-07-07 11:35:52 +0200

    Added rover class.

--- include/Rover.h
+++ include/Rover.h
@@ -1,8 +1,10 @@
 #ifndef ROVER_H_
 #define ROVER_H_
 
+#include <iostream>
 #include <queue>
 #include <cmath>
+using std::cout;
 using std::queue;
 
 #define PI 3.14159265
@@ -10,10 +12,10 @@ using std::queue;
 enum class Move{L, R, M};
 
 struct Position {
-    int X;
-    int Y;
+    int x;
+    int y;
     int theta;      // degrees
-    Position(): X(0), Y(0), theta(0)  {}
+    Position(): x(0), y(0), theta(0)  {}
 };
 
 class Grid {
@@ -34,10 +36,78 @@ public:
     }
 };
 
+class Rover {
+    Position position;
+    queue<Move> commands;
+    Grid *grid;
 
+    int normalizeTheta(int theta){
+        theta = theta % 360 ;
+        if(theta < 0) {
+            theta += 360;
+        }
+        return theta;
+    }
 
+    void executeCommand(Move command) {
+        switch (command)
+        {
+        case Move::L :
+            this->position.theta = normalizeTheta(this->position.theta + 90);
+            break;
+        case Move::R :
+            this->position.theta = normalizeTheta(this->position.theta - 90);
+            break;
+        case Move::M :
+            int newX = this->position.x + round(cos(this->position.theta * PI / 180)) ;
+            int newY = this->position.y + round(sin(this->position.theta * PI / 180)) ;
+            if (grid->isValidPosition(newX, newY)) {
+                this->position.x = newX;
+                this->position.y = newY;
+            } 
+            else {
+                cout << "Invalid move !! \n";    
+            }
+            break;
+        default:
+            cout << "command not found !! " ;
+            break;
+        }
+    }
+    
+public:
+    Rover (Position position) {
+        this->position = position;
+    }
+    void setCommands(queue<Move> commands) {
+        this->commands = commands;
+    }
+    void setGrid(Grid *grid) {
+        this->grid = grid;
+    }
+    void printPosition() {}
 
+    static int convertDirToTheata(const char dir) const {
+        if      (dir == 'E') return 0;
+        else if (dir == 'N') return 90;
+        else if (dir == 'W') return 180;
+        else if (dir == 'S') return 270;
+        else {
+            cout << " Direction not found !! \n";
+            return -1;
+        }
+    }
+
+    static char convertThetaToDir(const int theta) const {
+        if (theta == 0  || theta == 360) return 'E';
+        else if (theta == 90)   return 'N';
+        else if (theta == 180)  return 'W';
+        else if (theta == 270)  return 'S';
+        else {
+            cout << "theta is out of scope !! \n" ;
+            return 'x';
+        }
+    }
+};
 
 # endif // ROVER_H_
-
-
